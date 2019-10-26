cb9bef1bce3964c4b43e2f039201545c9f4a350e
voilalex <ilya.vouk@gmail.com>
2019-10-26 23:48:10 +0300

    Add SSD family files

---
+++ src/SSD/_ssd.py
@@ -0,0 +1,6 @@
+from model_template import ObjectDetectionNet
+
+
+class SSD(ObjectDetectionNet):
+    # TODO: implement SSD
+    raise NotImplementedError()
---
+++ src/SSD/model.py
@@ -0,0 +1,6 @@
+"""
+This module collects all the
+models of SSD family in one place.
+"""
+
+from ._ssd import SSD
