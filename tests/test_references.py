825b42bfaff18c14453f576977e57d89f38875f6
voilalex <ilya.vouk@gmail.com>
2019-10-27 18:35:47 +0300

    Update test routines specific for my platform

--- tests/test_references.py
+++ tests/test_references.py
@@ -8,13 +8,18 @@ parent_dir = os.path.dirname(script_dir)
 sys.path.insert(0, parent_dir)
 
 from data.references import DATASETS
+from src.utils.testutils import *
 
 
+@allow_MoyaPizama_constrains
 class TestReferences:
+
+    @MoyaPizama_specific_method
     def test_coco_train(self):
         assert os.path.exists(DATASETS['coco']['train']['images'])
         assert os.path.exists(DATASETS['coco']['train']['labels'])
 
+    @MoyaPizama_specific_method
     def test_coco_test(self):
         assert os.path.exists(DATASETS['coco']['test']['images'])
         assert os.path.exists(DATASETS['coco']['test']['labels'])
