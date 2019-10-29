ec28fd3477cc088abe18ef35ce4543a2c5256f7b
Ilya <ilya.vouk@gmail.com>
2019-10-29 17:32:31 +0300

    Add configuration file

---
+++ src/config.py
@@ -0,0 +1,7 @@
+IMAGE_SHAPE = (448, 448, 3)  # shape of input image
+CELL_SIZE = 7  # size of cell
+BOXES_PER_CELL = 2  # number of boxes per cell
+
+# This constant is used
+# throughout label parsing
+MAX_BB_COUNT = 100  # max count of bounding boxes
