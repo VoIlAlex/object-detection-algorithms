195b7cbd9861c4f221849103a682ab52d1a773ed
Ilya <ilya.vouk@gmail.com>
2019-10-29 17:33:57 +0300

    Add net methods to ObjectDetectionNet

--- src/model_template.py
+++ src/model_template.py
@@ -2,3 +2,15 @@
 class ObjectDetectionNet:
     def __init__(self):
         pass
+
+    def fit_generator(self, generator, validation_dataб):
+        return self._model.fit_generator(
+            generator=generator,
+            validation_data=validation_data
+        )
+
+    def evaluate_generator(self, generator):
+        return self._model.evaluate_generator(generator)
+
+    def predict_generator(self, generator):
+        return self._model.predict_generator(generator)
