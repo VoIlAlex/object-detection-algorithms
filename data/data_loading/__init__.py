92a0bfaf4381beee4a7ac6f1852a0efe0669668e
Ilya <ilya.vouk@gmail.com>
2019-10-29 17:32:15 +0300

    File collector for data loading module

---
+++ data/data_loading/__init__.py
@@ -0,0 +1,13 @@
+# data generators
+from .coco_style_data_generator import CocoStyleDataGenerator
+
+# label parsing routines
+from .label_parsing import parse_class_and_bounding_box
+
+# label parsers
+from .label_parsing import LabelParser
+from .label_parsing import DefaultLabelParser
+
+# image parsers
+from .image_parsing import ImageParser
+from .image_parsing import DefaultImageParser
