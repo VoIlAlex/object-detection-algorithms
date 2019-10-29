9ce935ce7751ad711e04dd488efba12c26b2ca3b
Ilya <ilya.vouk@gmail.com>
2019-10-29 22:39:52 +0300

    Add ObjectDetectionNet to file-collector

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,10 @@ This module collects all the
 classes and function of the project.
 """
 
+# base class for all
+# the detectors
+from .model_template import ObjectDetectionNet
+
 # models
 from .RCNN import *
 from .SSD import *
