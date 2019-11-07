dc50264d1a20087f31bb9d6d6082a5ca04c6ffe1
Ilya <ilya.vouk@gmail.com>
2019-11-07 22:20:07 +0300

    Update third-party script

--- pytorch_modelsize.py
+++ pytorch_modelsize.py
@@ -3,9 +3,10 @@ import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
 
+
 class SizeEstimator(object):
 
-    def __init__(self, model, input_size=(1,1,32,32), bits=32):
+    def __init__(self, model, input_size=(1, 1, 32, 32), bits=32):
         '''
         Estimates the size of PyTorch models in memory
         for a given input size
@@ -19,8 +20,8 @@ class SizeEstimator(object):
         '''Get sizes of all parameters in `model`'''
         mods = list(self.model.modules())
         sizes = []
-        
-        for i in range(1,len(mods)):
+
+        for i in range(1, len(mods)):
             m = mods[i]
             p = list(m.parameters())
             for j in range(len(p)):
@@ -31,7 +32,9 @@ class SizeEstimator(object):
 
     def get_output_sizes(self):
         '''Run sample input through each layer to get output sizes'''
-        input_ = Variable(torch.FloatTensor(*self.input_size), volatile=True)
+        with torch.no_grad():
+            # , volatile=True)
+            input_ = Variable(torch.FloatTensor(*self.input_size))
         mods = list(self.model.modules())
         out_sizes = []
         for i in range(1, len(mods)):
@@ -41,14 +44,14 @@ class SizeEstimator(object):
             input_ = out
 
         self.out_sizes = out_sizes
-        return
+        return self.out_sizes
 
     def calc_param_bits(self):
         '''Calculate total number of bits to store `model` parameters'''
         total_bits = 0
         for i in range(len(self.param_sizes)):
             s = self.param_sizes[i]
-            bits = np.prod(np.array(s))*self.bits
+            bits = np.prod(np.array(s)) * self.bits
             total_bits += bits
         self.param_bits = total_bits
         return
@@ -58,15 +61,15 @@ class SizeEstimator(object):
         total_bits = 0
         for i in range(len(self.out_sizes)):
             s = self.out_sizes[i]
-            bits = np.prod(np.array(s))*self.bits
+            bits = np.prod(np.array(s)) * self.bits
             total_bits += bits
         # multiply by 2 for both forward AND backward
-        self.forward_backward_bits = (total_bits*2)
+        self.forward_backward_bits = (total_bits * 2)
         return
 
     def calc_input_bits(self):
         '''Calculate bits to store input'''
-        self.input_bits = np.prod(np.array(self.input_size))*self.bits
+        self.input_bits = np.prod(np.array(self.input_size)) * self.bits
         return
 
     def estimate_size(self):
@@ -78,5 +81,5 @@ class SizeEstimator(object):
         self.calc_input_bits()
         total = self.param_bits + self.forward_backward_bits + self.input_bits
 
-        total_megabytes = (total/8)/(1024**2)
+        total_megabytes = (total / 8) / (1024**2)
         return total_megabytes, total
