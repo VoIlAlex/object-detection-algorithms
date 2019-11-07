7dfa3fb72c51c07792e45fb4ffea0d0022f9ba3b
Ilya <ilya.vouk@gmail.com>
2019-11-07 22:32:42 +0300

    Update file collector of YOLO family

--- src/YOLO/__init__.py
+++ src/YOLO/__init__.py
@@ -3,7 +3,14 @@ This module collects all the
 models of YOLO family in one place.
 """
 
-
+# version 1
 from ._yolo import YOLO
+from ._yolo import YOLO_pytorch
+from ._yolo import YOLO_keras
+
+
+# version 2
 from ._yolov2 import YOLOv2
+
+# version 3
 from ._yolov3 import YOLOv3
