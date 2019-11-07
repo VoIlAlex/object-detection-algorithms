480deb449114dca646a109ea87c8f812b865a40e
Ilya <ilya.vouk@gmail.com>
2019-11-07 22:33:24 +0300

    Add train/evaluate/predict methods for object detection base class

--- src/model_template.py
+++ src/model_template.py
@@ -3,6 +3,15 @@ class ObjectDetectionNet:
     def __init__(self):
         pass
 
+    def fit(self, X, y):
+        return self._model.fit(X, y)
+
+    def evaluate(self, X_test, y_test):
+        return self._model.evaluate(X_test, y_test)
+
+    def predict(self, X):
+        return self._model.predict(X)
+
     def fit_generator(self, generator, validation_data):
         return self._model.fit_generator(
             generator=generator,
