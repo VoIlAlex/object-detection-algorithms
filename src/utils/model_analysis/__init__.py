89f12878419e6da17b549fe7a3bd5edca9ab872e
voilalex <ilya.vouk@gmail.com>
2019-11-12 18:45:58 +0300

    Add model_analysis file-collector

---
+++ src/utils/model_analysis/__init__.py
@@ -0,0 +1,14 @@
+# Utilities
+from .utils import Visualizer
+
+# Different types of research
+from .research import Research
+from .research import ModelResearch
+
+# Research items
+from .research_items import ResearchItem
+from .research_items import AccuracyItem
+from .research_items import LossPrintItem
+from .research_items import LossVisualizationItem
+from .research_items import CurrentIterationItem
+from .research_items import ModelConfigurationItem
