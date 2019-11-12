202dfb8260871a35573439101ea33a912d6b0574
voilalex <ilya.vouk@gmail.com>
2019-11-12 18:44:53 +0300

    Add research items

---
+++ src/utils/model_analysis/research_items.py
@@ -0,0 +1,201 @@
+import os
+from .utils import Visualizer
+
+
+class ResearchItem:
+    """Base class for all the research
+    items.
+    """
+
+    def print(self, research_path, *args, **kwargs):
+        raise NotImplementedError(
+            'print(...) method should be implemented '
+            'by all the childs of ResearchItem class'
+        )
+
+
+class CurrentIterationItem(ResearchItem):
+    def __init__(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+
+    def print(self, research_path, *args, **kwargs):
+
+        #
+        # extract arguments
+        #
+
+        # user provided arguments
+        print_end = self._kwargs.get('print_end', '\n')
+        internal_path = self._kwargs.get('internal_path', 'report.txt')
+        iteration_modulo = self._kwargs.get('iteration_modulo', 1)
+
+        # framework provided arguments
+        epoch = kwargs.get('epoch', None)
+        iteration = kwargs.get('iteration', None)
+
+        # get out if necessary
+        if iteration % iteration_modulo != 0:
+            return
+
+        # path to file with the report
+        file_path = os.path.join(
+            research_path,
+            internal_path
+        )
+
+        # write item to the file
+        with open(file_path, 'a') as f:
+            print('[{:5d}{:5d}] '.format(
+                epoch, iteration), file=f, end=print_end)
+
+
+class AccuracyItem(ResearchItem):
+    def __init__(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+
+    def print(self, research_path, *args, **kwargs):
+
+        #
+        # extract arguments
+        #
+
+        # user provided arguments
+        print_end = self._kwargs.get('print_end', '\n')
+        internal_path = self._kwargs.get('internal_path', 'report.txt')
+        iteration_modulo = self._kwargs.get('iteration_modulo', 1)
+
+        # framework provided arguments
+        iteration = kwargs.get('iteration', None)
+        accuracy = kwargs.get('accuracy', None)
+
+        # get out if necessary
+        if iteration % iteration_modulo != 0:
+            return
+
+        # path to file with the report
+        file_path = os.path.join(
+            research_path,
+            internal_path
+        )
+
+        # write item to the file
+        with open(file_path, 'a') as f:
+            print('Accuracy: {:.5f} '.format(accuracy),
+                  file=f,
+                  end=print_end)
+
+
+class LossPrintItem(ResearchItem):
+    def __init__(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+
+    def print(self, research_path, *args, **kwargs):
+
+        #
+        # extract arguments
+        #
+
+        # user provided arguments
+        print_end = self._kwargs.get('print_end', '\n')
+        internal_path = self._kwargs.get('internal_path', 'report.txt')
+        iteration_modulo = self._kwargs.get('iteration_modulo', 1)
+
+        # framework provided arguments
+        iteration = kwargs.get('iteration', None)
+        loss = kwargs.get('loss', None)
+
+        # get out if necessary
+        if iteration % iteration_modulo != 0:
+            return
+
+        # path to file with the report
+        file_path = os.path.join(
+            research_path,
+            internal_path
+        )
+
+        # write item to the file
+        with open(file_path, 'a') as f:
+            print('Loss: {:.5f} '.format(loss),
+                  file=f,
+                  end=print_end)
+
+
+class LossVisualizationItem(ResearchItem):
+    def __init__(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+        func_descriptors = [
+            {
+                'name': 'train',
+                'color': 'r'
+            }
+        ]
+        self._visualizer = Visualizer(func_descriptors, 'Iterations', 'Loss')
+
+    def print(self, research_path, *args, **kwargs):
+
+        # user provided arguments
+        print_end = self._kwargs.get('print_end', '\n')
+        internal_path = self._kwargs.get('internal_path', 'report.txt')
+        iteration_modulo = self._kwargs.get('iteration_modulo', 1)
+
+        # framework provided arguments
+        iteration = kwargs.get('iteration', None)
+        epoch = kwargs.get('epoch', None)
+        loss = kwargs.get('loss', None)
+
+        # convert the loss
+        # to the right format
+        loss = loss.cpu().detach().numpy()
+
+        if iteration % iteration_modulo == 0:
+            self._visualizer.update({
+                'train': {
+                    'x': iteration,
+                    'y': loss
+                }
+            })
+        self._visualizer.redraw()
+
+
+class ModelConfigurationItem(ResearchItem):
+    def __init__(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+
+    def print(self, research_path, *args, **kwargs):
+        #
+        # extract arguments
+        #
+
+        # user provided arguments
+        print_end = self._kwargs.get('print_end', '\n')
+        internal_path = self._kwargs.get('internal_path', 'report.txt')
+
+        # framework provided arguments
+        epoch = kwargs.get('epoch', 0)
+        iteration = kwargs.get('iteration', 0)
+
+        # it should be printed only once
+        if epoch != 0 or iteration != 0:
+            return
+
+        model = kwargs.get('model', None)
+        optimizer = kwargs.get('optimizer', None)
+        criterion = kwargs.get('criterion', None)
+
+        # path to file with the report
+        file_path = os.path.join(
+            research_path,
+            internal_path
+        )
+
+        # write item to the file
+        with open(file_path, 'a') as f:
+            print(model, file=f)
+            print(optimizer, file=f)
+            print(criterion, file=f, end=print_end)
