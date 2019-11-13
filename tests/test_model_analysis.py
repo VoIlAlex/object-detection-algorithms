a979ca942bed8184184ae474cb060c03fa61bea1
Ilya <ilya.vouk@gmail.com>
2019-11-13 09:19:49 +0300

    Add construction tests for research items

--- tests/test_model_analysis.py
+++ tests/test_model_analysis.py
@@ -23,12 +23,19 @@ class TestResearchItems:
             pass
 
     class TestCurrentIterationItem:
-        def test_current_iteration_item_default(self):
+
+        def test_current_iteration_item_construction(self):
+            cia = analysis.CurrentIterationItem()
+            cia = analysis.CurrentIterationItem(absolute_iteration=False)
+            cia = analysis.CurrentIterationItem(absolute_iteration=True)
+
+        def test_current_iteration_item_iteration_epoch(self):
             research_path = '.temp'
             cia = analysis.CurrentIterationItem()
             kwargs = {
                 'epoch': 10,
-                'iteration': 12
+                'iteration': 12,
+                'iterations_per_epoch': 20
             }
 
             # create research file
@@ -53,11 +60,47 @@ class TestResearchItems:
             # remove temp file
             os.remove(report_path)
 
+        def test_current_iteration_item_absolute_iteration(self):
+            research_path = '.temp'
+            cia = analysis.CurrentIterationItem(absolute_iteration=True)
+            kwargs = {
+                'epoch': 10,
+                'iteration': 12,
+                'iterations_per_epoch': 20
+            }
+
+            # create research file
+            if not os.path.exists(research_path):
+                os.makedirs(research_path)
+
+            # clear the report file
+            report_path = os.path.join(
+                research_path,
+                'report.txt'
+            )
+            open(report_path, 'w').close()
+
+            # print current iteration and epoch
+            cia.print(research_path, **kwargs)
+
+            # check the report file
+            lines = [line.rstrip() for line in open(
+                report_path, 'r').readlines() if line != '']
+            assert 'Iteration:        212' in lines
+
+            # remove temp file
+            os.remove(report_path)
+
     class TestAccuracyItem:
 
-        def test_accuracy_print_item_default(self):
-            research_path = '.temp'
+        def test_accuracy_print_item_construction(self):
             api = analysis.AccuracyPrintItem()
+            api = analysis.AccuracyPrintItem(watch_test=False)
+            api = analysis.AccuracyPrintItem(watch_test=True)
+
+        def test_accuracy_print_item_with_test(self):
+            research_path = '.temp'
+            api = analysis.AccuracyPrintItem(watch_test=True)
             kwargs = {
                 'accuracy_train': 1,
                 'accuracy_test': 0.5
@@ -85,9 +128,74 @@ class TestResearchItems:
             # remove temp file
             os.remove(report_path)
 
+        def test_accuracy_print_item_without_test(self):
+            research_path = '.temp'
+            api = analysis.AccuracyPrintItem()
+            kwargs = {
+                'accuracy_train': 1,
+                'accuracy_test': 0.5
+            }
+
+            # create research file
+            if not os.path.exists(research_path):
+                os.makedirs(research_path)
+
+            # clear the report file
+            report_path = os.path.join(
+                research_path,
+                'report.txt'
+            )
+            open(report_path, 'w').close()
+
+            # print current iteration and epoch
+            api.print(research_path, **kwargs)
+
+            # check the report file
+            lines = [line.rstrip() for line in open(
+                report_path, 'r').readlines() if line != '']
+            assert 'Accuracy:   1.00' in lines
+
+            # remove temp file
+            os.remove(report_path)
+
     class TestLossPrintItem:
 
-        def test_loss_print_item_default(self):
+        def test_loss_print_item_construction(self):
+            lpi = analysis.LossPrintItem()
+            lpi = analysis.LossPrintItem(watch_test=True)
+            lpi = analysis.LossPrintItem(watch_test=False)
+
+        def test_loss_print_item_with_test(self):
+            research_path = '.temp'
+            lpi = analysis.LossPrintItem(watch_test=True)
+            kwargs = {
+                'loss_train': 1,
+                'loss_test': 2
+            }
+
+            # create research file
+            if not os.path.exists(research_path):
+                os.makedirs(research_path)
+
+            # clear the report file
+            report_path = os.path.join(
+                research_path,
+                'report.txt'
+            )
+            open(report_path, 'w').close()
+
+            # print current iteration and epoch
+            lpi.print(research_path, **kwargs)
+
+            # check the report file
+            lines = [line.rstrip() for line in open(
+                report_path, 'r').readlines() if line != '']
+            assert 'Loss: [  1.00,  2.00]' in lines
+
+            # remove temp file
+            os.remove(report_path)
+
+        def test_loss_print_item_without_test(self):
             research_path = '.temp'
             lpi = analysis.LossPrintItem()
             kwargs = {
@@ -112,7 +220,7 @@ class TestResearchItems:
             # check the report file
             lines = [line.rstrip() for line in open(
                 report_path, 'r').readlines() if line != '']
-            assert 'Loss: [  1.00,  2.00]' in lines
+            assert 'Loss:   1.00' in lines
 
             # remove temp file
             os.remove(report_path)
