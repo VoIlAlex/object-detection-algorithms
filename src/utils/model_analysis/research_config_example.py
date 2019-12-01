2681f802fad8aa36b4798eb937b9fd8132a0f2de
voilalex <ilya.vouk@gmail.com>
2019-12-01 22:10:40 +0300

    Add example of the research config

---
+++ src/utils/model_analysis/research_config_example.py
@@ -0,0 +1,22 @@
+import src.utils.model_analysis as analysis
+
+
+RESEARCH_SESSION_CONFIG = [
+    {
+        'construction': {
+            'model': None,
+            'optimizer': None,
+            'criterion': None,
+            'watch_test': None,
+            'research_scheme': None
+        },
+        'session': {
+            'train_dataloader': None,
+            'test_dataloader': None,
+            'dataset_name': None,
+            'epochs': None,
+            'iteration_modulo': None
+        }
+
+    }
+]
