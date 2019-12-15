d64b0575fff45ba483776db8e05cb952091e13f4
voilalex <ilya.vouk@gmail.com>
2019-12-15 20:35:50 +0300

    Yolo training routine completed

--- main.py
+++ main.py
@@ -1,81 +1,56 @@
+"""
+    Works for keras.
+"""
+
 from src import *
+from data.data_loading import VOCDataGenerator
 from data.references import DATASETS
-import src.config as cfg
-import os
-import sys
-
 import torch
-import torchvision
-import torchvision.transforms as transforms
-
-# class for model folder path generation
 from src.utils.path_generation import ModelPathGenerator
-
-# Import default research
-from src.utils.model_analysis import ModelResearch
-
-# Import research items
-from src.utils.model_analysis import LossPrintItem
-from src.utils.model_analysis import LossVisualizationItem
-from src.utils.model_analysis import CurrentIterationItem
-from src.utils.model_analysis import ModelConfigurationItem
-
-EPOCHS_COUNT = 100
+import src.config as config
 
 if __name__ == "__main__":
-    # model, dataset, loss, optimizer
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(
-            mean=(0.485, 0.456, 0.406),
-            std=(0.229, 0.224, 0.225)
-        )
-    ])
+    # train_generator = CocoStyleDataGenerator(
+    #     images_dir=DATASETS['coco']['train']['images'],
+    #     labels_dir=DATASETS['coco']['train']['labels'],
+    #     names_path=DATASETS['coco']['names']
+    # )
+    # test_generator = CocoStyleDataGenerator(
+    #     images_dir=DATASETS['coco']['test']['images'],
+    #     labels_dir=DATASETS['coco']['test']['labels'],
+    #     names_path=DATASETS['coco']['names']
+    # )
 
-    trainset = torchvision.datasets.CIFAR100(root='./data/datasets', train=True,
-                                             download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=32,
-                                              shuffle=True, num_workers=2)
-    testset = torchvision.datasets.CIFAR100(root='./data/datasets', train=False,
-                                            download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=32,
-                                             shuffle=False, num_workers=2)
-
-    net = torchvision.models.MobileNetV2(num_classes=100).cuda()
-
-    optimizer = torch.optim.SGD(
-        params=net.parameters(),
-        lr=0.001,
-        momentum=0.9
+    train_generator = VOCDataGenerator(
+        images_dir=DATASETS['voc']['images_dir'],
+        label_txt="data/labels/voc2007.txt",
+        image_shape=(448, 448, 3),
+        grid_size=7,
+        max_bb_count=2,
+        num_classes=20,
+        to_fit=True
     )
-    criterion = torch.nn.CrossEntropyLoss()
 
-    # generate path
-    path_generator = ModelPathGenerator(
-        root_path=cfg.MY_PRETRAINED_MODELS_PATH
+    test_generator = VOCDataGenerator(
+        images_dir=DATASETS['voc']['images_dir'],
+        label_txt="data/labels/voc2007.txt",
+        image_shape=(448, 448, 3),
+        grid_size=7,
+        max_bb_count=2,
+        num_classes=20,
+        to_fit=False
     )
+
+    model = YOLO(B=2, S=7, C=20)
+    model.fit_generator(
+        generator=train_generator,
+        validation_data=test_generator,
+        channels_first=True
+    )
+
+    path_generator = ModelPathGenerator(config.MY_PRETRAINED_MODELS_PATH)
     path = path_generator.generate_path(
-        model=net,
-        optimizer=optimizer,
-        criterion=criterion,
-        epochs=EPOCHS_COUNT,
-        dataset_name='CIFAR100',
-        makedirs=True
+        model=model,
+        session=1
     )
-
-    # research
-    research = ModelResearch(
-        research_path=path,
-        research_scheme=[
-            ModelConfigurationItem(),
-            CurrentIterationItem(print_end=' ', iteration_modulo=10),
-            LossPrintItem(iteration_modulo=10),
-            LossVisualizationItem(iteration_modulo=10)
-        ],
-        model=net,
-        optimizer=optimizer,
-        criterion=criterion
-    )
-
-    research.start_research_session(
-        trainloader, testloader, epochs=EPOCHS_COUNT, iteration_modulo=50)
+    torch.save(YOLO.state_dict(), path)
